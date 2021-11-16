ad476d1891f48be325c2eef4d1738e332c1ec9d7
fishbone04 <91872431+fishbone04@users.noreply.github.com>
2021-11-16 21:53:34 +0100

    Add files via upload

--- Drogkriget/js/script.js
+++ Drogkriget/js/script.js
@@ -1,7 +1,3 @@
-function myFunction() {
-    document.getElementById("demo").style.display="none";
-}
-
 function buttondown(buttonid) {
   document.getElementById(buttonid).style.boxShadow="inset 0px 10px 9px -10px black, 0px 5px 15px -5px black";
 }
@@ -14,9 +10,6 @@ function meny() {
   var menydropCompStyle = window.getComputedStyle(menydropdown);
   var menydropDisplay = menydropCompStyle.getPropertyValue("display");
 
-
-/*document.getElementById("demo").innerHTML = menydropDisplay;*/
-
   if (menydropDisplay == "none") {
     document.getElementById("Menydrop").style.display="flex";
     document.getElementById("meny").style.borderBottomLeftRadius="0px";
@@ -26,5 +19,43 @@ function meny() {
     document.getElementById("Menydrop").style.display="none";
     document.getElementById("meny").style.borderBottomLeftRadius="6px";
     document.getElementById("meny").style.borderBottomRightRadius="6px";
+  }
+}
+function menycontent() {
+
+var meny = document.getElementById("meny");
+var menyheight = meny.offsetHeight;
+var menywidth = meny.offsetWidth;
+var windowheight = window.innerHeight;
+var linksfontresize = 2/19 * menywidth;
+var linkswidthresize = 15/19 * menywidth;
+var titelfontresize = 3/19 * menywidth;
+document.getElementById("demo").innerHTML = menyheight + " " + menywidth + " " + linksfontresize + " " + windowheight;
+
+  if (menyheight > 130) {
+    meny.style.justifyContent="center";
+  }
+  else if ((menyheight == 130)&&(menywidth > 180) ) {
+    meny.style.justifyContent="space-between";
+  }
+  if (menywidth <= 380) {
+    document.getElementById("links").style.fontSize=linksfontresize + "pt";
+    document.getElementById("links").style.width=linkswidthresize + "px";
+    document.getElementById("titel").style.fontSize=titelfontresize + "pt";
+
+    document.getElementById("links").style.marginLeft="0px";
+    document.getElementById("links").style.marginRight="0px";
+    document.getElementById("titel").style.marginLeft="0px";
+    document.getElementById("titel").style.marginRight="0px";
+  }
+  else {
+    document.getElementById("links").style.fontSize="40pt";
+    document.getElementById("links").style.width="300px";
+    document.getElementById("titel").style.fontSize="60pt";
+
+    document.getElementById("links").style.marginLeft="40px";
+    document.getElementById("links").style.marginRight="40px";
+    document.getElementById("titel").style.marginLeft="40px";
+    document.getElementById("titel").style.marginRight="40px";
   }
 }
