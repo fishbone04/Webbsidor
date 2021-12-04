dd5d327067802a8076cd00e839e6bfb22d9f59f6
fishbone04 <91872431+fishbone04@users.noreply.github.com>
2021-12-04 19:42:44 +0100

    Add files via upload

--- Drogkriget/js/script.js
+++ Drogkriget/js/script.js
@@ -1,3 +1,31 @@
+const n = ["M", "Me", "Mex", "Mexi", "Mexik", "Mexiko", "Mexikos", "Mexikos D", "Mexikos Dr", "Mexikos Dro", "Mexikos Drog", "Mexikos Drogk", "Mexikos Drogkri", "Mexikos Drogkrig"];
+let i = 0;
+var font = 1;
+setInterval(titleupd, 300);
+
+function titleupd(){
+  document.title = n[i];
+  i += 1;
+  if (i > 13) {
+    i=0;
+  }
+}
+  /*const test = ["kyckling", "potatis"]
+  var test2 = 0;
+  document.getElementById("demo").innerHTML=test[test2];
+  test2 += 1;*/
+
+/*  switch (font) {
+    case 1:
+    document.getElementById("body").style.fontFamily="arial";
+    font = 2;
+    break;
+    case 2:
+    document.getElementById("body").style.fontFamily="impact";
+    font = 1;
+  }
+
+}*/
 function buttondown(buttonid) {
   /*document.getElementById(buttonid).style.boxShadow="inset 0px 10px 9px -10px black, 0px 5px 15px -5px black";*/
   document.getElementById(buttonid).style.filter="brightness(50%)";
@@ -6,6 +34,9 @@ function buttondown(buttonid) {
 function buttonup(buttonid) {
 /*document.getElementById(buttonid).style.boxShadow="inset 0px -10px 9px -10px black, 0px 5px 20px -5px black";*/
   document.getElementById(buttonid).style.filter="brightness(1)";
+}
+function buttonhoveron(buttonid) {
+  document.getElementById(buttonid).style.filter="brightness(75%)";
 }
 function meny() {
   var menydropdown = document.getElementById("Menydrop");
@@ -16,24 +47,28 @@ function meny() {
   if (menydropDisplay == "none") {
     document.getElementById("Menydrop").style.display="flex";
     document.getElementById("meny").style.borderBottomLeftRadius="0px";
-    menyimage.src = "bilder/menu_close.svg";
+    menyimage.src = "../../bilder/menu_close.svg";
   }
   else {
     document.getElementById("Menydrop").style.display="none";
     document.getElementById("meny").style.borderBottomLeftRadius="6px";
-    menyimage.src = "bilder/menu_open.svg";
+    menyimage.src = "../../bilder/menu_open.svg";
   }
+}
+function hem(){
+  location.href="../home"
 }
 function menycontent() {
 
 var meny = document.getElementById("meny");
 var menyheight = meny.offsetHeight;
 var menywidth = meny.offsetWidth;
+var menydropwidth = document.getElementById("Menydrop").offsetWidth;
 var windowheight = window.innerHeight;
 var linksiconresize = 1/7 * menywidth;
 var linkswidthresize = 10/21 * menywidth;
 var titelfontresize = 1/12 * menywidth;
-document.getElementById("demo").innerHTML = menyheight + " " + menywidth + " " + linksiconresize + " " + windowheight;
+/*document.getElementById("demo").innerHTML = menyheight + " " + menywidth + " " + linksiconresize + " " + windowheight;*/
 
   if (menyheight > 130) {
     meny.style.justifyContent="center";
@@ -62,5 +97,11 @@ document.getElementById("demo").innerHTML = menyheight + " " + menywidth + " " +
     document.getElementById("links").style.marginRight="40px";
     document.getElementById("titel").style.marginLeft="40px";
     document.getElementById("titel").style.marginRight="40px";
+  }
+  if (menydropwidth >= menywidth) {
+    document.getElementById("meny").style.borderBottomRightRadius="0px";
+  }
+  else {
+    document.getElementById("meny").style.borderBottomRightRadius="6px";
   }
 }
